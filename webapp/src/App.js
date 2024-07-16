110b7137b3e83c1911bc6857559442433ddc8b6a
Abhishek Upadhyay <megablazikenabhishek@gmail.com>
2024-07-16 23:28:04 +0530

    initalized dsahboard page

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import LogInPage from './pages/loginPage/loginPage';
 import NotFoundPage from './pages/notFoundPage/notFoundPage';
+import DashboardPage from './pages/dashboardPage/dashboardPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<LogInPage />}/>
+        <Route path='/home' element={<DashboardPage />}/>
         <Route path='*' element={<NotFoundPage />}/>
       </Routes>
     </BrowserRouter>
---
+++ webapp/src/pages/dashboardPage/dashboardPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import DashboardHeader from "../../components/dashboardHeader/dashboardHeader";
+import BottomNav from "../../components/bottomNav/bottomNav";
+
+function DashboardPage(params) {
+    return (
+        <>
+            <DashboardHeader/>
+            <BottomNav/>
+        </>
+    );
+}
+
+export default DashboardPage;
