a965fcb94b88be108f59c843d91c962d1d8aa4f0
Abhishek Upadhyay <megablazikenabhishek@gmail.com>
2024-07-17 23:45:56 +0530

    fs

--- webapp/src/pages/dashboardPage/dashboardPage.js
+++ webapp/src/pages/dashboardPage/dashboardPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import DashboardHeader from "../../components/dashboardHeader/dashboardHeader";
 import BottomNav from "../../components/bottomNav/bottomNav";
+import DashboardBody from "../../components/dashboardBody/dashboardBody";
 
 function DashboardPage(params) {
     return (
         <>
             <DashboardHeader/>
+            <DashboardBody/>
             <BottomNav/>
         </>
     );
