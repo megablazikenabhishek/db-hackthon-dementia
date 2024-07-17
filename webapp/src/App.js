2d6ee0ec730b1dc781d5e352354c1f26254f3c2d
Shrutii9 <punjabishruti9@gmail.com>
2024-07-17 16:53:12 +0530

    login and signup

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import LogInPage from './pages/loginPage/loginPage';
+// import LogInPage from './pages/loginPage/loginPage';
 import NotFoundPage from './pages/notFoundPage/notFoundPage';
 import DashboardPage from './pages/dashboardPage/dashboardPage';
+import Login from './components/Login/Login';
+import SignIn from './components/SignIn/SignIn';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<DashboardPage />}/>
-        <Route path='/login' element={<LogInPage />}/>
+        <Route path='/login' element={<Login />}/>
         <Route path='*' element={<NotFoundPage />}/>
+        <Route path='/signin' element={<SignIn />}/>
       </Routes>
     </BrowserRouter>
   );
