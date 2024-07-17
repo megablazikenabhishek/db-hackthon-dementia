dddbc1fa9307fd5caf8dea3d744812c39bb80c33
Abhishek Upadhyay <megablazikenabhishek@gmail.com>
2024-07-18 01:48:26 +0530

    fs

--- webapp/src/pages/dashboardPage/dashboardPage.js
+++ webapp/src/pages/dashboardPage/dashboardPage.js
@@ -5,11 +5,11 @@ import DashboardBody from "../../components/dashboardBody/dashboardBody";
 
 function DashboardPage(params) {
     return (
-        <>
+        <div className="bg-[#F7F7F7]">
             <DashboardHeader/>
             <DashboardBody/>
             <BottomNav/>
-        </>
+        </div>
     );
 }
 
